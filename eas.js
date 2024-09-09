de32bb5149c300014a13acb7ae9f7f9261b50f9b
Battletoadster <72815818+Battletoadster@users.noreply.github.com>
2024-09-09 22:53:38 +0200

    Add regen button, listener and resize grid

--- eas.js
+++ eas.js
@@ -1,13 +1,33 @@
 const gridContainer = document.querySelector(".grid-container");
+const regenBtn = document.querySelector(".regen-btn");
+let gridSize;
 
-function initializeGrid(rows, cols){
-    for(let i = 0; i < rows; i++){
-        for (let j = 0; j < cols; j++){
-            let square = document.createElement("div");
+regenBtn.addEventListener("click", () => {
+    do {
+        gridSize = parseInt(prompt("What should be the width and height of the grid be? (1-100)"));
+    }
+    while (isNaN(gridSize) || typeof gridSize !== "number" || gridSize <= 0 || gridSize > 100);
+
+    while(gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    initializeGrid(gridSize, gridSize);
+
+});
+
+function initializeGrid(rows, cols) {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const square = document.createElement("div");
             square.classList.add("square");
+            square.addEventListener("mouseover", () => {
+                square.classList.add("colored");
+            });
+            square.style.flexBasis = `calc(100%/${rows})`;
             gridContainer.append(square);
         }
     }
 }
 
-initializeGrid(16,16);
+initializeGrid(16, 16);
